67f06ca66cd132129ca5e1e02cea0a254cc57f3c
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-01 00:15:26 +0300

    yep

--- BIOS.lua
+++ BIOS.lua
@@ -36,7 +36,7 @@ function gui.clear()
 	term.clear()
 end
 
-function gui.drawMain(tOS)
+function gui.drawMain()
 	gui.clear()
 	term.setCursorPos(1,3)
 	print(layout)
@@ -44,7 +44,7 @@ function gui.drawMain(tOS)
 	term.write(OS[currentOS][1])
 	for i = 1, #OS do
 		term.setCursorPos(8,i+6)
-		term.write(OS[i][1])
+		term.write(i..") "..OS[i][1])
 	end
 	term.setCursorPos(19,16)
 	term.write(timeLeft)
@@ -73,20 +73,29 @@ defaultOS = loadDefault()
 currentOS = findCurrent()
 while true do 
 	gui.drawMain()
-	local event = {os.pullEvent("char")}
-	print(event[1])
-	print(event[2])
-	if tonumber(event[2]) then
-		print("cow")
-		if OS[tonumber(event[2])] then
-			print"hell"
-			toBoot = tonumber(event[2])
+	local timerID = os.startTimer(1)
+	local event = {os.pullEvent()}
+	if event[1] == "key" then
+		os.cancelTimer(timerID)
+		if 2 <= event[2] and event[2] <= 11 then
+			event[2] = event[2] == 11 and 0 or event[2] - 1
+			if OS[event[2]] then
+				toBoot = event[2]
+				break
+			end
+		elseif event[2] == keys.up then
+			currentOS = currentOS - 1
+			currentOS = currentOS == 0 and #OS or currentOS
+		elseif event[2] == keys.down then
+			currentOS = currentOS + 1
+			currentOS = currentOS == #OS + 1 and 1 or currentOS
+		elseif event[2] == keys.enter then
+			toBoot = currentOS
 			break
 		end
-	else
-
+	elseif event[1] == "timer" and event[2] == timerID then
+		timeLeft = timeLeft - 1
 	end
-	os.pullEvent("mouse_click")
 end
 
 dofile(OS[toBoot][2])
