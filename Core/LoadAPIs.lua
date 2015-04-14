e31bf6a0ce56f86597d37b5e61c72537e90f7de6
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-14 19:58:09 +0300

    Changed load api mechanichs

--- Core/LoadAPIs.lua
+++ Core/LoadAPIs.lua
@@ -1,6 +1,31 @@
 --Code--
 local apis = fs.list("OmniOS/API/")
 local dirs = {}
+function loadAPI( _sPath )
+    local sName = fs.getName( _sPath )
+    local tEnv = {}
+    setmetatable( tEnv, { __index = _G } )
+    local fnAPI, err = loadfile( _sPath )
+    if fnAPI then
+        setfenv( fnAPI, tEnv )
+        local ok, err = pcall( fnAPI )
+        if not ok then
+            printError( err )
+            return false
+        end
+    else
+        printError( err )
+        return false
+    end
+    
+    local tAPI = {}
+    for k,v in pairs( tEnv ) do
+        tAPI[k] =  v
+    end
+    
+    _G[sName:match("[^%.]+")] = tAPI    
+    return true
+end
 for i,v in pairs(apis) do
 	if fs.isDir("OmniOS/API/"..v) then
 		dirs[i] = true
@@ -10,7 +35,7 @@ for i,v in pairs(dirs) do
 	apis[i] = nil
 end
 for i,v in pairs(apis) do
-	os.loadAPI("OmniOS/API/"..v)
+	loadAPI("OmniOS/API/"..v)
 	term.setTextColor(colors.black)
 	print(v)
 end
