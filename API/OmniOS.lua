19ceb3f62829e993212813e9e8f678d76ed5036a
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-21 20:41:10 +0300

    Tracked the error down

--- API/OmniOS.lua
+++ API/OmniOS.lua
@@ -1,2 +1,27 @@
 version = "alpha"
-tag = "15w15b"
+tag = "15w16a"
+loadAPI = function(_sPath)
+	local sName = fs.getName( _sPath )
+	local tEnv = {}
+	setmetatable( tEnv, { __index = _G } )
+	local fnAPI, err = loadfile( _sPath )
+	if fnAPI then
+		setfenv( fnAPI, tEnv )
+		local ok, err = pcall( fnAPI )
+		if not ok then
+			printError( err )
+			return false
+		end
+	else
+		printError( err )
+		return false
+	end
+	
+	local tAPI = {}
+	for k,v in pairs( tEnv ) do
+		tAPI[k] =  v
+	end
+	
+	_G[sName:match("[^%.]+")] = tAPI	
+	return true
+end
