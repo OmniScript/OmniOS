3f96fe8395ae22bf5a17cf379f212360bd105670
TheOnlyCreator <wassil.janssen@gmail.com>
2015-04-26 23:18:52 +0300

    Add: Debug commands

--- Programs/Debug.app/Main.lua
+++ Programs/Debug.app/Main.lua
@@ -1,7 +1,73 @@
-term.setCursorPos(1,1)
-print("OmniOS > Debug")
-print(">")
-System.newTask("FileX","FileX")
+--[[
+	OmniOS debug
+	app
+]]--
+--System.newTask("FileX","FileX")
+
+
+--[[
+    Modified version of Shell
+]]--
+
+promptColour = colours.yellow
+textColour = colours.white
+bgColour = colours.black
+
+local function run(command,...)
+	if fs.exists("OmniOS/Programs/Debug.app/Commands/"..command) and not fs.isDir("OmniOS/Programs/Debug.app/Commands/"..command) then
+		local func, err = loadfile("OmniOS/Programs/Debug.app/Commands/"..command)
+		print(err)
+		setfenv(func,_G)
+		if ... then 
+			func(unpack({...}))
+		else
+			func()
+		end
+	else
+		print("Command is non-existent! Pay more attention when writting!")
+	end
+end
+
+local function tokenise( ... )
+    local sLine = table.concat( { ... }, " " )
+	local tWords = {}
+    local bQuoted = false
+    for match in string.gmatch( sLine .. "\"", "(.-)\"" ) do
+        if bQuoted then
+            table.insert( tWords, match )
+        else
+            for m in string.gmatch( match, "[^ \t]+" ) do
+                table.insert( tWords, m )
+            end
+        end
+        bQuoted = not bQuoted
+    end
+    return tWords
+end
+
+local function shellrun( ... )
+	local tWords = tokenise( ... )
+	local sCommand = tWords[1]
+	if sCommand then
+		return run( sCommand, unpack( tWords, 2 ) )
+	end
+	return false
+end
+
+term.setBackgroundColor( bgColour )
+term.setTextColour( promptColour )
+print("OmniOS/Debug")
+term.setTextColour( textColour )
+
+-- Read commands and execute them
+local tCommandHistory = {}
 while true do
-	sleep(1)
+    term.setBackgroundColor( bgColour )
+    term.setTextColour( promptColour )
+    write("> ")
+    term.setTextColour( textColour )
+
+    local sLine = read( nil, tCommandHistory )
+    table.insert( tCommandHistory, sLine )
+    shellrun( sLine )
 end
